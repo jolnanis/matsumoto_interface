d3b167c38a652a510adc23b2680b4a7ec83beb51
studiolab <studiolab@studiolab-PC>
2016-11-09 17:01:34 +0900

    First commit

---
+++ public_html/testSQL.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var connection = new ActiveXObject("ADODB.Connection") ;
+
+var connectionstring="Data Source=<server>;Initial Catalog=<catalog>;User ID=<user>;Password=<password>;Provider=SQLOLEDB";
+
+connection.Open(connectionstring);
+var rs = new ActiveXObject("ADODB.Recordset");
+
+rs.Open("SELECT * FROM table", connection);
+rs.MoveFirst
+while(!rs.eof)
+{
+   document.write(rs.fields(1));
+   rs.movenext;
+}
+
+rs.close;
+connection.close;
